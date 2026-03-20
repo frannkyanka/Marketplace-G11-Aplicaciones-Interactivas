69f01a498338e101b96f9190c771b745b24a8678
Matias Meza <matiasmeza77@gmail.com>
2026-03-20 15:49:28 -0300

    Cambios repository Mysql

--- src/main/java/com/uade/tpo/demo/repository/CategoryRepository.java
+++ src/main/java/com/uade/tpo/demo/repository/CategoryRepository.java
@@ -1,34 +1,14 @@
 package com.uade.tpo.demo.repository;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.uade.tpo.demo.entity.Category;
 
-public class CategoryRepository {
-    private ArrayList<Category> categories;
 
-    public CategoryRepository() {
-        categories = new ArrayList<Category>(
-                Arrays.asList(Category.builder().description("Electronica").id(1).build(),
-                        Category.builder().description("Cocina").id(2).build(),
-                        Category.builder().description("Gaming").id(3).build()));
-    }
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    public ArrayList<Category> getCategories() {
-        return this.categories;
-    }
 
-    public Optional<Category> getCategoryById(int categoryId) {
-        return this.categories.stream().filter(m -> m.getId() == categoryId).findAny();
-    }
-
-    public Category createCategory(int newCategoryId, String description) {
-        Category newCategory = Category.builder()
-                .description(description)
-                .id(newCategoryId).build();
-        this.categories.add(newCategory);
-        return newCategory;
-    }
+    
 }
